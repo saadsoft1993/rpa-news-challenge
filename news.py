3e99e1131805f95a22f0e273269417fcd99880f5
Saad bin Abid <31469212+saadsoft1993@users.noreply.github.com>
2023-05-24 21:21:29 +0500

    Update: modify the script

--- news.py
+++ news.py
@@ -3,7 +3,6 @@ import re
 import time
 import os
 import shutil
-
 from RPA.Browser.Selenium import Selenium
 from RPA.Excel.Files import Files
 from RPA.HTTP import HTTP
@@ -11,24 +10,18 @@ from RPA.Archive import Archive
 from SeleniumLibrary.errors import ElementNotFound
 from dateutil.relativedelta import relativedelta
 from selenium.webdriver.common.by import By
-
 from news_locators import NewsLocators
 from models import News
 from config import OUTPUT
-
-
 class FreshNews:
     """
     A class to scrape fresh news from a website.
-
     Args:
         search_phrase (str): The search phrase for news articles.
         sections (list): The list of sections to filter news articles.
         months (int): The number of months to retrieve news articles from.
-
     """
-
-    def __init__(self, search_phrase, sections, months):
+    def __init__(self, search_phrase, months, sections=None):
         self.browser = Selenium()
         self.base_url = 'https://www.nytimes.com/'
         self.locators = NewsLocators()
@@ -39,11 +32,9 @@ class FreshNews:
         self.files = Files()
         self.http = HTTP()
         self.lib = Archive()
-
     def setup(self):
         """
         Set up directories for storing output.
-
         """
         print('Setting up directories.')
         if not os.path.exists(OUTPUT):
@@ -55,29 +46,23 @@ class FreshNews:
             elif os.path.isdir(file_path):
                 shutil.rmtree(file_path)
         os.mkdir(f'{OUTPUT}/imgs')
-
     def open_site(self):
         """
         Open the news website.
-
         """
         print('Accessing the news website.')
         self.browser.open_headless_chrome_browser(self.base_url)
-
     def search_text(self):
         """
         Perform a search with the specified search phrase.
-
         """
         print(f'Searching with phrase: {self.search_phrase}')
         self.browser.click_element_if_visible(self.locators.search_button)
         self.browser.press_keys(self.locators.search_input, self.search_phrase)
         self.browser.press_keys(self.locators.search_input, 'RETURN')
-
     def select_section(self):
         """
         Select the specified sections to filter news articles.
-
         """
         if self.sections:
             print(f'Selecting sections: {", ".join(self.sections)}')
@@ -86,25 +71,20 @@ class FreshNews:
                 try:
                     self.browser.click_element_if_visible(self.locators.section_select.format(section=section))
                 except Exception as e:
-                    pass
+                    print(f'Failed to select section {section} with error: {str(e)}')
             self.browser.click_element_if_visible(self.locators.section_button)
-
     def select_newest(self):
         """
         Select the filter for newest news.
-
         """
         print('Selecting filter for newest news.')
         self.browser.wait_until_page_contains_element(self.locators.sort_button)
         self.browser.select_from_list_by_value(self.locators.sort_button, 'newest')
-
     def get_total_results_count(self):
         """
         Get the total number of results for the search.
-
         Returns:
             int: The total number of results.
-
         """
         raw_string = self.browser.find_element(self.locators.total_result).text
         try:
@@ -113,25 +93,20 @@ class FreshNews:
             total_value_string = '0'
         total_value_int = int(total_value_string.replace(' ', '').replace(',', ''))
         return total_value_int
-
     def get_date(self):
         """
         Get the date range for the search.
-
         Returns:
             tuple: The start and end dates in the format (to_date, from_date).
-
         """
         if self.months == 0:
             self.months = 1
         to_date = datetime.datetime.now()
         from_date = to_date - relativedelta(months=self.months)
         return to_date.strftime('%m/%d/%Y'), from_date.strftime('%m/%d/%Y')
-
     def set_date_range(self):
         """
         Set the date range for the search.
-
         """
         to_date, from_date = self.get_date()
         print(f'Setting date range from {from_date} to {to_date}.')
@@ -140,14 +115,11 @@ class FreshNews:
         self.browser.input_text_when_element_is_visible(self.locators.start_date, from_date)
         self.browser.input_text_when_element_is_visible(self.locators.end_date, to_date)
         self.browser.press_keys(self.locators.end_date, 'RETURN')
-
     def read_news(self, news_index):
         """
         Read and store the details of a news article.
-
         Args:
             news_index (int): The index of the news article.
-
         """
         tries = 3
         while tries:
@@ -168,7 +140,6 @@ class FreshNews:
         file_name = f'image{news_index}'
         file_path = f'{OUTPUT}/imgs/{file_name}'
         self.http.download(image_url, file_path)
-
         news_record = News(
             title=title,
             date=date,
@@ -179,11 +150,9 @@ class FreshNews:
             filename=file_name
         )
         self.news_list.append(news_record)
-
     def start(self):
         """
         Start the scraping process.
-
         """
         self.setup()
         self.open_site()
@@ -204,11 +173,9 @@ class FreshNews:
                     pass
                 self.browser.click_element(self.locators.show_more)
             self.read_news(index)
-
     def end(self):
         """
         Finish the scraping process and store the collected data.
-
         """
         print('Creating Excel with collected news data.')
         self.files.create_workbook(f'{OUTPUT}/Latest News.xlsx', 'xlsx', f'News')
